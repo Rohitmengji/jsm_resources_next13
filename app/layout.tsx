68e2b1e7f2849f22b20064bf61a14d78bead1785
rohitmengji <rohitmengjih@gmail.com>
2023-10-13 00:36:44 +0530

    added meta data

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,28 @@
-import './globals.css'
-import type { Metadata } from 'next'
+import "./globals.css";
+import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: 'JS Mastery',
-  description: 'JS Mastery Resources',
+  title: "JS Mastery",
+  description: "JS Mastery Resources",
   other: {
-    'theme-color': '#0d1117',
+    "theme-color": "#0d1117",
     "color-scheme": "dark only",
-    "twitter:image": 'https://i.ibb.co/d6TXxB2/homepage-thumbnail.jpg',
+    "twitter:image": "https://i.ibb.co/d6TXxB2/homepage-thumbnail.jpg",
     "twitter:card": "summary_large_image",
-    "og:url": "jsmastery.pro",
-    "og:image": 'https://i.ibb.co/d6TXxB2/homepage-thumbnail.jpg',
+    "og:url": "https://javascript-mastery-pro.vercel.app/", // Update with your actual URL
+    "og:image": "https://i.ibb.co/d6TXxB2/homepage-thumbnail.jpg", // Update with your actual image URL
     "og:type": "website",
-  }
-}
+  },
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className="min-h-screen bg-black-100 font-poppins">{children}</body>
+    <html lang='en'>
+      <body className='min-h-screen bg-black-100 font-poppins'>{children}</body>
     </html>
-  )
+  );
 }
